3bfa77db6464925d43460ec384c48fea642a5471
Anych2 <anuar123@mail.ru>
2022-08-03 22:13:04 +0000

    Change days seconds

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,7 +7,7 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-        int days = seconds / 86399;
+        int days = seconds / 86400;
         int hours = (seconds - days * 86400) / 3600;
         int minutes = (seconds - days * 86400 - hours * 3600) / 60;
         int sec_to_show = seconds - hours * 3600 - minutes * 60 - days * 86400;
